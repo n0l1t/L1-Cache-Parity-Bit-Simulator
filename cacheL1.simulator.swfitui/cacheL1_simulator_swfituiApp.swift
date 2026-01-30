412a97471616fd3e6fd08c5e10fa14af8ceb1d6e
n0lit <n0rthl1ght@yandex.ru>
2026-01-30 12:16:13 +0300

    Initial Commit

---
+++ cacheL1.simulator.swfitui/cacheL1_simulator_swfituiApp.swift
@@ -0,0 +1,17 @@
+//
+//  cacheL1_simulator_swfituiApp.swift
+//  cacheL1.simulator.swfitui
+//
+//  Created by Максим Фурсов on 1/30/26.
+//
+
+import SwiftUI
+
+@main
+struct cacheL1_simulator_swfituiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cacheL1.simulator.swfituiTests/cacheL1_simulator_swfituiTests.swift
@@ -0,0 +1,17 @@
+//
+//  cacheL1_simulator_swfituiTests.swift
+//  cacheL1.simulator.swfituiTests
+//
+//  Created by Максим Фурсов on 1/30/26.
+//
+
+import Testing
+@testable import cacheL1_simulator_swfitui
+
+struct cacheL1_simulator_swfituiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
